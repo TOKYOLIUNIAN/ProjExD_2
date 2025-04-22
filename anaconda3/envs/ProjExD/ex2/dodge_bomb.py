ae0f9a91c558921b910caab641b4304e5af70ef1
TOKYO_LIUNIAN <c0a24274@edu.teu.ac.jp>
2025-04-22 14:52:20 +0900

    練習問題2

--- anaconda3/envs/ProjExD/ex2/dodge_bomb.py
+++ anaconda3/envs/ProjExD/ex2/dodge_bomb.py
@@ -36,7 +36,7 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]          #后改的
+        sum_mv = [0, 0]          #后改
 
         DELTA = {
             pg.K_UP : (0, -5),
